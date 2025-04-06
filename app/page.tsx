0be8dd544239b7ed45989c9bd6cb421c1614d0d5
Mesut GENEZ <genezmesut@gmail.com>
2025-04-06 20:49:04 +0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Button } from '@/components/ui/button'
 
 const HomePage = () => {
   return (
     <div>
       <h1>Home Page</h1>
+
+      <Button variant="destructive">Click me</Button>
     </div>
   )
 }
